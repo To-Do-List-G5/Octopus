5f7e20632f7d7e6566fe5449f3e1848d3962a9e6
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-05-22 12:26:21 +0300

    Added new file

---
+++ Front-end/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Navbar from "./Layout/Sidebar";
+function App() {
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  };
+  return (
+    <>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />{" "}
+          <Route path="/Navbar" element={<Navbar />} />{" "}
+        </Routes>
+      </Router>{" "}
+    </>
+  );
+}
+
+export default App;
